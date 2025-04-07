2b04d2a87dbe46e8fae1cae455b82849ebc1bc3a
Đinh Ngọc Hiếu <Dinhngochieud@gmail.com>
2025-04-07 17:35:04 +0700

    Update

--- src/common/database.js
+++ src/common/database.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 const config = require("../../config/db");
 module.exports = () =>{
-    // mongoose.set('strictQuery', false);
+    mongoose.set('strictQuery', false);
     mongoose
         .connect(config.mongodb.uri)
         .then(()=> console.log(" Connected! "));
